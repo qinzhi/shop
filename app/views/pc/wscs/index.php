1758ca09125da83c20c32df95f9088f1c7304529
zhaosen <1414568046@qq.com>
2014-05-15 21:48:27 +0800

    dasd

---
+++ app/views/pc/wscs/index.php
@@ -0,0 +1,405 @@
+<?php require_once (REALPATH.'/include/Class/util/DateUtil.php');?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>校园电子商务网站</title>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo SOURCE_ROOT;?>/wscs/css/tzsc.css" />
+    <script src="<?php echo SOURCE_ROOT;?>/jyjz/js/indexwscs.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+
+<div id="container">
+
+<div class="head">
+    <ul>
+
+        <li><a href="#">联系我们</a></li>
+        <li><a href="#">收藏本站</a></li>
+        <li><a href="#">同德官网</a></li>
+        <li class="head_z"><a href="#">注册</a></li>
+        <li class="head_d"><a href="#">登入</a></li>
+        <li class="head_h">欢迎光临本站</li>
+
+    </ul>
+</div>
+<div class="header">
+    <div class="logo fl"><img src="<?php echo SOURCE_ROOT;?>/jyjz/images/logo.jpg" width="450px" height="120px"/></div>
+    <div class="header_ss fr">
+        <input class="fl" name="text" type="text"/><a href="#" class="fr"><img src="<?php echo SOURCE_ROOT;?>/jyjz/images/search.jpg" /></a>
+        <div class="header_ss_x fr"><strong>热门搜索：</strong>
+            <a href="#">面包</a>
+            <a href="#">方便面</a>
+            <a href="#">篮球</a>
+            <a href="#">二手电脑</a>
+            <a href="#">自行车</a>
+            <a href="#">兼职</a>
+            <a href="#">精美礼品</a>
+            <a href="#">更多...</a>	 </div>
+
+    </div>
+</div>
+<div class="menu">
+    <ul>
+        <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/shop/index">首页</a></li>
+
+
+
+
+
+
+        <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/wscs/index">网上超市</a>
+            <div class="pos_a">
+                <ul id="nav">
+
+                    <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/wscs/index">生活用品</a></li>
+                    <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/wscs/index">精美礼品</a></li>
+
+                </ul>
+            </div>
+        </li>
+        <li><a href="#">校园团购</a>
+            <div class="pos_a">
+                <ul id="nav">
+
+                    <li><a href="#">食品</a></li>
+                    <li><a href="#">电影票</a></li>
+                    <li><a href="#">生活用品</a></li>
+                    <li><a href="#">体育用品</a></li>
+                </ul>
+            </div>
+        </li>
+        <li><a href="#">跳蚤市场</a>
+            <div class="pos_a">
+                <ul id="nav">
+
+                    <li><a href="#">自行车</a></li>
+                    <li><a href="#">电动车</a></li>
+                    <li><a href="#">IT产品</a></li>
+
+
+                </ul>
+            </div>
+        </li>
+        <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/jyjz/index">就业兼职</a>
+            <div class="pos_a">
+                <ul id="nav">
+
+                    <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/jyjz/index">兼职招骋</a></li>
+                    <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/jyjz/index">名企招骋</a></li>
+                    <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/jyjz/index">投递简历</a></li>
+
+
+                </ul>
+            </div>
+
+        </li>
+        <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/courier/donate">快递在线</a>
+            <div class="pos_a">
+                <ul id="nav">
+
+                    <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/courier/donate">快递查询</a></li>
+                    <li><a href="<?php echo APP_WEB_INDEX_ROOT;?>/courier/donate">快递订购</a></li>
+
+
+
+                </ul></div>
+        </li>
+        <li><a href="#">论坛交流</a>
+            <div class="pos_a">
+                <ul id="nav">
+
+                    <li><a href="#">兴趣交流</a></li>
+                    <li><a href="#">威客帮帮团</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</div>
+<div class="menu_x cl">
+    <div class="menu_x_x fl">
+        <strong>您当前的位置是：</strong>跳蚤市场
+
+
+    </div>
+
+    <div class="menu_x_mar fl"><marquee scrollamount=4 direction="left" behavior="scroll" onmouseover=this.stop() onmouseout=this.start()>这个写最新通知!</marquee>
+    </div>
+</div>
+
+<div class="content">
+<div class="ldftcontent fl">
+
+    <h3><span>商品服务分类</span></h3>
+    <dl>
+        <dt><a href="category-1-b0.html"><strong>网上超市</strong></a></dt>
+        <dd>     <a href="category-6-b0.html">饼干</a>
+
+            <a href="category-9-b0.html">面类</a>
+
+            <a href="category-8-b0.html">面包粥点</a>
+
+            <a href="category-17-b0.html">休闲食品</a>
+
+            <a href="category-4-b0.html">烟酒槟榔</a>
+
+            <a href="category-16-b0.html">糖果</a>
+
+            <a href="category-7-b0.html">熟食</a>
+
+            <a href="category-14-b0.html">香肠火腿</a>
+
+        </dd>
+    </dl>
+
+    <dl>
+        <dt><a href="category-1-b0.html"><strong>校园团购</strong></a></dt>
+        <dd>     <a href="category-6-b0.html">饼干</a>
+
+            <a href="category-9-b0.html">面类</a>
+
+            <a href="category-8-b0.html">面包粥点</a>
+
+            <a href="category-17-b0.html">休闲食品</a>
+
+            <a href="category-4-b0.html">烟酒槟榔</a>
+
+            <a href="category-16-b0.html">糖果</a>
+
+            <a href="category-7-b0.html">熟食</a>
+
+            <a href="category-14-b0.html">香肠火腿</a>
+
+        </dd>
+    </dl>
+    <dl>
+        <dt><a href="category-1-b0.html"><strong>跳蚤市场</strong></a></dt>
+        <dd>     <a href="category-6-b0.html">饼干</a>
+
+            <a href="category-9-b0.html">面类</a>
+
+            <a href="category-8-b0.html">面包粥点</a>
+
+            <a href="category-17-b0.html">休闲食品</a>
+
+            <a href="category-4-b0.html">烟酒槟榔</a>
+
+            <a href="category-16-b0.html">糖果</a>
+
+            <a href="category-7-b0.html">熟食</a>
+
+            <a href="category-14-b0.html">香肠火腿</a>
+
+        </dd>
+    </dl>
+    <dl>
+        <dt><a href="category-1-b0.html"><strong>就业兼职</strong></a></dt>
+        <dd>     <a href="category-6-b0.html">饼干</a>
+
+            <a href="category-9-b0.html">面类</a>
+
+            <a href="category-8-b0.html">面包粥点</a>
+
+            <a href="category-17-b0.html">休闲食品</a>
+
+            <a href="category-4-b0.html">烟酒槟榔</a>
+
+            <a href="category-16-b0.html">糖果</a>
+
+            <a href="category-7-b0.html">熟食</a>
+
+            <a href="category-14-b0.html">香肠火腿</a>
+
+        </dd>
+    </dl>
+    <dl>
+        <dt><a href="category-1-b0.html"><strong>快递在线</strong></a></dt>
+        <dd>     <a href="category-6-b0.html">饼干</a>
+
+            <a href="category-9-b0.html">面类</a>
+
+            <a href="category-8-b0.html">面包粥点</a>
+
+            <a href="category-17-b0.html">休闲食品</a>
+
+            <a href="category-4-b0.html">烟酒槟榔</a>
+
+            <a href="category-16-b0.html">糖果</a>
+
+            <a href="category-7-b0.html">熟食</a>
+
+            <a href="category-14-b0.html">香肠火腿</a>
+
+        </dd>
+    </dl>
+    <dl>
+        <dt><a href="category-1-b0.html"><strong>论坛交流</strong></a></dt>
+        <dd>     <a href="category-6-b0.html">饼干</a>
+
+            <a href="category-9-b0.html">面类</a>
+
+            <a href="category-8-b0.html">面包粥点</a>
+
+            <a href="category-17-b0.html">休闲食品</a>
+
+            <a href="category-4-b0.html">烟酒槟榔</a>
+
+            <a href="category-16-b0.html">糖果</a>
+
+            <a href="category-7-b0.html">熟食</a>
+
+            <a href="category-14-b0.html">香肠火腿</a>
+
+        </dd>
+    </dl>
+
+
+
+</div>
+<div class="rightbox fr">
+    <div class="tit"><h3>商品刷选</h3></div>
+    <div class="cont">
+        <span>全部：</span>
+
+        <strong>
+            <a href="#">自行车</a>
+            <a href="#">电动车</a>
+            <a href="#">IT产品</a>
+        </strong>
+    </div>
+
+
+</div>
+<div class="rightcontent fr">
+    <div class="tit"><h3>全部商品</h3></div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="cl" style="padding-top:12px;"><hr color="#EBEBEB" size="1px"/></div>
+
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="cl" style="padding-top:12px;"><hr color="#EBEBEB" size="1px"/></div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+    <div class="tdcs fl">
+
+        <a href="#"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/k41500067.jpg" alt="香飘飘奶茶32g" width="150px" height="150px;" /></a><br />
+        <p><a href="#" title="香飘飘奶茶32g"> 香飘飘奶茶32g</a></p>
+        <p class="mon"><strong>￥3元</strong></p>
+
+        <a href="#" ><img src="<?php echo SOURCE_ROOT;?>/wscs/images/tdcs.jpg" width="99" height="23"></a>
+    </div>
+</div>
+<div class="cl"></div>
+<div class="syye">
+    <ul class="xyy">
+        <li><a href="#">上一页</a></li>
+        <li><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">...</a></li>
+        <li><a href="#">下一页</a></li>
+        <li><input type="text" style="width:25px; height:20px;"/></li>
+        <li><input type="button" style="height:22px;" value="跳转"/></li>
+    </ul>
+</div>
+<div class="cl"></div><br />
+<hr color="#FF0000" size="1px" />
+
+</div>
+<div class="cl"></div>
+
+<div class="guanyu"><a href="#">关于本站</a><a href="#">服务条款</a><a href="#">帮助中心</a><a href="#">友情链接</a><a href="#">支付说明</a><a href="#">客服服务</a><a href="#">联系我们</a></div>
+
+<div class="banquan"><img src="<?php echo SOURCE_ROOT;?>/wscs/images/banquan.gif" /></div>
+
+
+</div>
+
+</body>
+</html>
